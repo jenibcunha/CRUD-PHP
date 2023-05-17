21578edeb6089e56c8a70e71194f6f8e9ad2ed44
jenibcunha <101344814+jenibcunha@users.noreply.github.com>
2023-05-16 22:04:18 -0300

    Update style.php

---
+++ style.php
@@ -0,0 +1,108 @@
+<style type="text/css">
+  /* index.php */
+
+
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .card {
+    width: 345px;
+    height: 449px;
+    background: #F4F6FB;
+    border: 1px solid white;
+    box-shadow: 10px 10px 64px 0px rgba(180, 180, 207, 0.75);
+    -webkit-box-shadow: 10px 10px 64px 0px rgba(186, 186, 202, 0.75);
+    -moz-box-shadow: 10px 10px 64px 0px rgba(208, 208, 231, 0.75);
+  }
+
+  .form {
+    padding: 35px;
+  }
+
+  .form_heading {
+    padding-bottom: 20px;
+    font-size: 21px;
+    color: #7878bd;
+  }
+
+  .field {
+    padding-bottom: 19px;
+  }
+
+  .input {
+    border-radius: 5px;
+    background-color: #e9e9f7;
+    padding: 9px;
+    width: 100%;
+    color: #7a7ab3;
+    border: 1px solid #dadaf7
+  }
+
+  .input:focus-visible {
+    outline: 1px solid #aeaed6;
+  }
+
+  .input::placeholder {
+    color: #bcbcdf;
+  }
+
+  label {
+    color: #B2BAC8;
+    font-size: 19px;
+    display: block;
+    padding-bottom: 9px;
+  }
+
+  .button {
+    background-color: #7878bd;
+    margin-top: 10px;
+    font-size: 19px;
+    padding: 7px 12px;
+    height: auto;
+    font-weight: 500;
+    color: white;
+    border: none;
+  }
+
+  .button:hover {
+    background-color: #5f5f9c;
+  }
+
+  /*listar-contatos.php*/
+
+  table {
+    border-collapse: collapse;
+    width: 100%;
+  }
+
+  th,
+  td {
+    text-align: left;
+    padding: 8px;
+  }
+
+  tr:nth-child(even) {
+    background-color: #f2f2f2
+  }
+
+  th {
+    background-color: #7878bd;
+    color: white;
+  }
+
+  .b-operacao {
+    background-color: #3C486B;
+    text-decoration: none;
+    width: 23%;
+    font-size: 29px;
+    padding: 9px 15px;
+    height: auto;
+    font-weight: 500;
+    color: white;
+    border: none;
+
+  }
+</style>
